924c87c6af5135b8847204c9f88f980f7ae060e4
Michael Castillo <maicol89@gmail.com>
2018-02-02 16:45:03 -0500

    friends and server files

---
+++ app/data/friends.js
@@ -0,0 +1,33 @@
+//MICHAEL FIX THIS FILE!!!
+
+//DONT FORGET!
+
+
+var friends = [
+	{
+		name: "Daniela Ryf",
+		photo: "https://tri-mag.de/files/styles/teaser_image_small/public/media/video/file/c4ca4/10cce956921d3b2df0b61539a97f1bcc.jpg?itok=82m7hvHQ",
+		scores: [#, #, #, #, #, #, #, #, #, #]
+	},{
+		name: "Sebastian Kienle",
+		photo: "https://www.tritime-magazin.de/wp-content/uploads/2016/09/Sebastian-Kienle.jpg",
+		scores: [#, #, #, #, #, #, #, #, #, #]
+	},{
+		name: "Neil Patrick",
+		photo: "https://pmcdeadline2.files.wordpress.com/2017/03/neil-patrick-harris-2.jpg?w=605",
+		scores: [#, #, #, #, #, #, #, #, #, #]
+	},{
+		name: "Jeff Dunham",
+		photo: "http://onwardstate.com/wp-content/uploads/2013/11/ccsu_dunham_controlledchaos_06.jpg",
+		scores: [#, #, #, #, #, #, #, #, #, #]
+	},{
+		name: "Kristen Bell",
+		photo: "https://vignette.wikia.nocookie.net/simpsons/images/9/9f/Kristen_Bell.jpg/revision/latest?cb=20150715121846",
+		scores: [#, #, #, #, #, #, #, #, #, #]
+	},{
+		name: "Emma Stone",
+		photo: "http://traceycunningham.com/wp-content/uploads/2015/12/landscape-1450730783-upload-2.png",
+		scores: [#, #, #, #, #, #, #, #, #, #]
+	}];
+
+module.exports = friends;
